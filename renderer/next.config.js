5f7cd7e7433b51bc650d43ea9e520f82ff3bc28c
Leo Lamprecht <mindrun@icloud.com>
2018-01-02 13:26:47 +0100

    Tinier config

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,14 +1,7 @@
-module.exports = {
-  webpack(config) {
-    config.target = 'electron-renderer'
-    return config
-  },
-  exportPathMap() {
-    // Let Next.js know where to find the entry page
-    // when it's exporting the static bundle for the use
-    // in the production version of your app
-    return {
-      '/start': { page: '/start' }
-    }
-  }
-}
+exports.webpack = config => Object.assign(config, {
+  target: 'electron-renderer'
+})
+
+exports.exportPathMap = () => ({
+  '/start': { page: '/start' }
+})
